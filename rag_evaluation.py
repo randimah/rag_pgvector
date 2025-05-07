9d32f7e11ff2ed35979529876745a9af2685f90d
Randima Hettiarachchi <randinuwangi@gmail.com>
2025-05-06 22:09:43 -0500

    added rag evaluaiton

---
+++ rag_evaluation.py
@@ -0,0 +1,68 @@
+import datasets
+import psycopg2
+from langchain_ollama import OllamaEmbeddings
+from langchain_ollama import ChatOllama
+from evalaution import evaluate_generated_response, load_eval_dataset
+
+"""
+
+Given the search query, retrieve the context from the vector DB, 
+augment the prompt and call a locally hosted LLM to generate the answer.
+
+"""
+
+conn = psycopg2.connect("dbname=VECTOR_DBNAME user=USER")
+cur = conn.cursor()
+
+ollama_embeddings = OllamaEmbeddings(
+    model="mxbai-embed-large",
+)
+
+llm = ChatOllama(model="llama3.2", temperature=0.1,)
+
+def search_documents(query, limit=5):
+    query_embedding = ollama_embeddings.embed_documents([query])[0]
+    cur.execute("""
+        SELECT content, source, embedding <-> %s AS distance
+        FROM documents
+        ORDER BY distance
+        LIMIT %s
+    """, (str(query_embedding), limit))
+    return cur.fetchall()
+
+# load evaluation dataset
+questions, answers = load_eval_dataset()
+
+# Perform a search
+doc_index = 3
+search_query = questions[doc_index]
+results = search_documents(search_query, limit=3)
+print(f"Search results for: '{search_query}'")
+
+page_contents = []
+for i, (content, source, distance) in enumerate(results, 1):
+    print(f"{i}. {source} (Distance: {distance:.4f})")
+    page_contents.append(content)
+
+# generate rag response
+RAG_PROMPT_TEMPLATE = """You are an assistant for question-answering tasks.
+    Use the following documents to answer the question.
+    If you don't know the answer, just say that you don't know.
+    Use three sentences maximum and keep the answer concise:
+    Question: {question}
+    Documents: {documents}
+    Answer:
+    """
+
+docs_content = "\n\n".join(page_content for page_content in page_contents)
+final_prompt = RAG_PROMPT_TEMPLATE.format(question=search_query,
+                                          documents=docs_content)
+response = llm.invoke(final_prompt)
+print('Generated answer:', response.content)
+
+evaluate_generated_response(
+    search_query,
+    response.content,
+    answers[doc_index],
+    page_contents
+)
